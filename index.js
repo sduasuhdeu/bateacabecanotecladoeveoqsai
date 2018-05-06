d3d41e15bcb24e8d70a0e3aab3e8bb66856ae2de
HyperGalactic <legendmccljr@gmail.com>
2018-05-06 13:23:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const superagent = require('superagent');
 const Webhook = require('webhook-discord');
-const LothusHook = new Webhook('https://discordapp.com/api/webhooks/442500342505144320/s3GYVlMTpArZrAc3LiYLLCByYXfNRY3H9P_2aAhJ4kB0DYnGsUunLnHyjcjG3CBhxyc0', 'Lothus - ChangeLog', )
+const LothusHook = new Webhook('https://discordapp.com/api/webhooks/442722550133555200/9Mgc56NN8_akGU4Osoe3PuynksScJ9iGCL4KOARQwE1ZkLUc2-tDuPfrlo43McNXOVHO', 'Lothus - ChangeLog', )
 
 const client = new Discord.Client();
 
@@ -60,10 +60,11 @@ client.on('message', msg =>{
 			mm = '0'+mm
 		} 
 		
-		getData = mm + '/' + dd + '/' + yyyy;
+		getData = dd + '/' + mm + '/' + yyyy;
 
 		LothusHook.custom("Lothus - Changelog",`:white_small_square: ${change}`,`${getData}`,"#65ff00", "");
-
+		
+		message.channel.send(":white_check_mark: Operação efetuada com Sucesso");
 	}
 
 
